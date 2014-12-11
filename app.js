4985a8c693e56601945684ac5310b14e4b06481f
Mateusz Dobek <mat.dobek@gmail.com>
2014-12-11 20:55:53 +0100

    commented mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/expressSampleProject');
+//mongoose.connect('mongodb://localhost/expressSampleProject');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,8 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //attach database to request
 app.use(function(req, res, next) {
-  req.db = mongoose.connection;
-  next();
+  //req.db = mongoose.connection;
+  //next();
 });
 
 app.use('/', routes);
